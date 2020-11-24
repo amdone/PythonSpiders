fbfe64d8f5fdcf79fcd0e2a44601d44f86e19512
amdone <34444201+amdone@users.noreply.github.com>
2020-11-24 16:55:52 +0800

    Update bili.py

--- bilidown/bili.py
+++ bilidown/bili.py
@@ -1,23 +1,49 @@
+import getopt
 import os
+import sys
 import time
 import json
 import requests
 from Sql_Bili import Sql_Bili
 
+path = './'
+cookie_path = './cookie.txt'
 
-def ft(longlonglongsequence:str):
-    return longlonglongsequence.replace('\n','</br>').replace("'","''")
+opts, args = getopt.getopt(sys.argv[2:], 'u:g:d:', ['up=', 'gd=', 'od=', 'gd', 'od', 'all'])
+
+for o, a in opts:
+    if o == '-u':
+        CloudName = a
+    if o == '-d':
+        CloudName = 'od'
+    if o == '-g':
+        CloudName = 'gd'
+
+print(CloudName)
+
+
+def ft(longlonglongsequence: str):
+    return longlonglongsequence.replace('\n', '</br>').replace("'", "''")
+
+
+def down_img(url, out_path):
+    kw = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+    }
+    if url == '':
+        return
+    r = requests.get('http://' + url, headers=kw)
+    with open(out_path, 'wb') as fd:
+        fd.write(r.content)
 
 
 class user:
     def __init__(self, space_no):
         self.space_no = space_no
-        def ft(longlonglongsequence:str):
-            return longlonglongsequence.replace('\n','</br>').replace("'","''")
 
     def get_vlist(self):
         av_list = []
-        video_set = [] # aid, created, title,  description, pic
+        video_list = []  # aid, created, title,  description, pic, mid, author
         pn = 1
         video_list_request_url = "http://api.bilibili.com/x/space/arc/search"
         stop = False
@@ -37,15 +63,74 @@ class user:
                 break
             for i, v in enumerate(vlist):
                 av_list.append(v['aid'])
-                video_set.append((v['aid'], v['created'], ft(v['title']), ft(v['description']), v['pic'][2:], v['mid']))
+                video_list.append(
+                    (v['aid'], v['created'], ft(v['title']), ft(v['description']), v['pic'][2:], v['mid'], v['author']))
             pn += 1
-        return av_list, video_set
+        return av_list, video_list
+
+
+class Down:
+    def __init__(self, space_no):
+        self.space_no = space_no
+        self.exits_videos = Sql_Bili(space_no).get_vlist()
+
+    def down_all_video(self, av_list, exits_avlist, dir_path='./'):
+        author = Sql_Bili(self.space_no).get_author()
+        try:
+            if not os.path.isdir(dir_path + author):
+                os.makedirs(dir_path + author)
+            if not os.path.isdir(dir_path + author + '/cover'):
+                os.makedirs(dir_path + author + '/cover')
+        except:
+            pass
+        else:
+            # dir_path += author + '/'
+            pass
+        count = 1
+        if cookie_path is None:
+            annie_cmd = "annie -o " + dir_path + author + "/ -p "
+        else:
+            annie_cmd = "annie -o " + dir_path + author + "/ -c " + cookie_path + " -p {}"
+        for av in av_list:
+            if av in exits_avlist:
+                continue
+            str_av = 'av' + str(av)
+            print("[" + str(count) + "]" + str_av + " start...")
+            os.system(annie_cmd.format(str_av))
+            print("[" + str(count) + "]" + str_av + " done!")
+            count += 1
+            # Sql_Bili(self.space_no).insert_video()
+            cover_url, title = Sql_Bili(self.space_no).get_cover(av)
+            img_type = cover_url.split('.')[-1]
+            down_img(cover_url, '{}/{}/cover/{}_av{}.{}'.format(dir_path, author, title, av, img_type))
+            # down_img(cover_url, dir_path + "/" + author + "/cover/" + title + str(av) + '.' + img_type)
+            if not CloudName == '':
+                cmd = "rclone copy '{0}{1}/' {2}:/uploads/bilibili/{1}/ -P".format(dir_path, author, CloudName)
+                print(cmd)
+                os.system(cmd)
+                cmd = "rm -rf {0}{1}/*.mp4 {0}{1}/cover/*".format(dir_path, author)
+                os.system(cmd)
+        if not CloudName == '':
+            cmd = "rm -rf {}".format(dir_path + author)
+            os.system(cmd)
+
+    def down_cover(self, url, out_path):
+        kw = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+        }
+        if url == '':
+            return
+        r = requests.get(url, headers=kw)
+        with open(out_path, 'wb') as fd:
+            fd.write(r.content)
 
 
 if __name__ == '__main__':
-    vl, vs = user(8366990).get_vlist()
-    vl.sort(reverse=True)
-    [print(i) for i in vs]
-    Sql_Bili(254).insert_video_from_set(vs)
-    # [Sql_Bili(125).insert_video_from_tuple(i) for i in vs]
-    print('{} 个视频！'.format(len(vs)))
+    space_id = sys.argv[1]
+    exits_vlist = Sql_Bili(space_id).get_vlist()
+    vl, vs = user(space_id).get_vlist()
+    print('Find {} videos！'.format(len(vl)))
+    # [print(i) for i in vl]
+    Sql_Bili(1).insert_video_from_list(vs)
+    print(Sql_Bili(space_id).get_author())
+    Down(space_id).down_all_video(vl, exits_vlist)
